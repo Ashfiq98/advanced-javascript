0a24dde2ebf1272e6bd2e8cde92c026ec4c6a563
Ashfiq98 <67517628+Ashfiq98@users.noreply.github.com>
2020-08-09 23:45:21 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy
+
+//0
+//''
+//NULL
+//NaN
+//undefined
+
+//Truthy
+
+//'0',' ',[],{}
+
+let num =0;
+if (num)
+    console.log('Condition is true');
+else
+    console.log('Condition is false');
